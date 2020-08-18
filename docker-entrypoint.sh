28c066c64336302bb6f9ae535c123719af8ebe2e
Eliseo247 <62765860+Eliseo247@users.noreply.github.com>
2020-08-17 21:41:58 -0600

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,54 +1,55 @@
-#GlassFish Server Open Source Edition 3.1.2 (build 23)
-#-----------------------------RECORDAR CREAR LAS VARIABLES DE ENTORNO--------------
-##AS_ADMIN_PASSWORD  Gla$fish2020
-#AS_ADMIN_ENABLE_SECURE
-#100GB /home/Digitalizados
-#10GB /usr/local/glassfish3
+#!/bin/sh
 
+if ! whoami &> /dev/null; then
+  if [ -w /etc/passwd ]; then
+    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
+  fi
+fi
 
+if [ "$1" = 'asadmin' ]; then
+    if [ "$AS_ADMIN_PASSWORD" ]; then
+        echo "AS_ADMIN_PASSWORD=" > /tmp/glassfishpwd
+        echo "AS_ADMIN_NEWPASSWORD=${AS_ADMIN_PASSWORD}" >> /tmp/glassfishpwd
 
-FROM openjdk:7u151-jdk-alpine
+        # Change the admin password
+        asadmin --user=admin --passwordfile=/tmp/glassfishpwd change-admin-password --domain_name domain1 && id
 
+       su - exemple
 
-# Set environment variables
-ENV GLASSFISH_PKG=/tmp/glassfish-3.1.2.2.zip \
-    GLASSFISH_URL=http://download.oracle.com/glassfish/3.1.2.2/release/glassfish-3.1.2.2.zip \
-    GLASSFISH_HOME=/usr/local/glassfish3 \
-    MD5=ae8e17e9dcc80117cb4b39284302763f \
-    PATH=$PATH:/usr/local/glassfish3/bin
+        # Log in the admin user
+        # 
+        # While using the `asadmin login` command is the proper way to do it,
+        # there doesn't seem to be a way to pipe input to respond to its
+        # interactive prompts for the username and password.
+        #
+        #   printf "admin\n${AS_ADMIN_PASSWORD}\n" | asadmin login
+        #
+        # Therefore, manually generate the .asadminpass file to "login".
+        
+        AS_ADMIN_PASSWORD_GFBASE64=`echo -n "$AS_ADMIN_PASSWORD" | base64`
+        echo "asadmin://admin@localhost:4848 $AS_ADMIN_PASSWORD_GFBASE64" > /root/.asadminpass && id
+su - exemple
+        chmod 600 /root/.asadminpass && id
+su - exemple
 
-# Download and install GlassFish
-RUN wget -q -O $GLASSFISH_PKG $GLASSFISH_URL && \
-    echo "$MD5 *$GLASSFISH_PKG" | md5sum -c - && \
-    unzip -o $GLASSFISH_PKG -d /usr/local && \
-    rm -f $GLASSFISH_PKG && \
-    \
-    # Remove Windows .bat and .exe files to save space
-    cd $GLASSFISH_HOME && \
-    find . -name '*.bat' -delete && \
-    find . -name '*.exe' -delete
+        if [ "$AS_ADMIN_ENABLE_SECURE" ]; then
+            echo "AS_ADMIN_PASSWORD=${AS_ADMIN_PASSWORD}" > /tmp/glassfishpwd
+            asadmin start-domain            
+            
+            asadmin --user=admin --passwordfile=/tmp/glassfishpwd enable-secure-admin && id
+su - exemple
+            
+            # Call asadmin at least once to establish a trust with the
+            # self-signed certificate for the admin API.
+            # asadmin --interactive=false version
 
-# Ports being exposed
-EXPOSE 4848 8080 8181
+            asadmin stop-domain
+        fi
 
-WORKDIR /usr/local/glassfish3
+        rm /tmp/glassfishpwd
+    fi
 
-# Copy in and set the entrypoint
-COPY docker-entrypoint.sh $GLASSFISH_HOME/
-RUN chmod 777 /usr/local/glassfish3/docker-entrypoint.sh
-RUN chgrp -R 0 /usr/local/glassfish3 && \
-    chmod -R g=u /usr/local/glassfish3
-    
-    RUN chmod g=u /etc/passwd
-  
-ENTRYPOINT ["/usr/local/glassfish3/docker-entrypoint.sh"]
-USER 1001
-# Start the GlassFish domain
-CMD ["asadmin", "start-domain", "--verbose"]
+    exec "$@"
+fi
 
-LABEL maintainer="King Chung Huang <kchuang@ucalgary.ca>" \
-      org.label-schema.schema-version="1.0" \
-      org.label-schema.name="GlassFish" \
-      org.label-schema.version="3.1.2.2" \
-      org.label-schema.url="https://glassfish.java.net" \
-      org.label-schema.vcs-url="https://github.com/ucalgary/docker-glassfish"
+exec "$@"
